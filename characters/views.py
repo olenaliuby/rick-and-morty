296bb1ee6079611369b1d06e5098e80ea500a88f
Olena Liubynetska <olena.liuby@gmail.com>
2023-10-26 15:09:35 +0100

    feat: Added DRF & 1st view — random character implemented

--- characters/views.py
+++ characters/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from characters.models import Character
+from characters.serializers import CharacterSerializer
+
+
+def get_random_character() -> Character:
+    pks = Character.objects.values_list("pk", flat=True)
+    random_pk = random.choice(pks)
+    return Character.objects.get(pk=random_pk)
+
+
+@api_view(["GET"])
+def get_random_character_view(request: Request) -> Response:
+    """Get random character from Rick & Morty world"""
+    random_character = get_random_character()
+    serializer = CharacterSerializer(random_character)
+    return Response(serializer.data, status=status.HTTP_200_OK)
